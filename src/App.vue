ce89b9ad39c8af0b553418ea27a21d19fb8cef6c
szl <ahhfszl@outlook.com>
2021-06-29 17:07:33 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app" class="my_project">
+    <img src="./assets/bg.jpg" style="height: 50%;width: 50%">
+<!--    <router-view/>-->
+  <h2>第九组 尚紫梁 从凌伟 谈染 窦浩云</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.my_project
+{
+  background-image: url("./assets/bg1.jpg");
+  background-size:cover;
+  height: 1080px;
+}
+</style>
